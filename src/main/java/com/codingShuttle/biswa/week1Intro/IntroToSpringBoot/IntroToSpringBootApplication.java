aed27653a12d4a4bd12162f72923db0a05715623
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2024-11-17 22:52:01 +0530

    Setting Up the Folder Structure

---
+++ src/main/java/com/codingShuttle/biswa/week1Intro/IntroToSpringBoot/IntroToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.biswa.week1Intro.IntroToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingShuttle/biswa/week1Intro/IntroToSpringBoot/IntroToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.biswa.week1Intro.IntroToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
